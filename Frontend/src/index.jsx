f327179271650c5169f5d0200fcdf42d0df9557f
bpieczek <bartosz.pieczek@pisz.to>
2025-07-20 03:53:59 +0200

    Real hqash router

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,7 +1,6 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { Router, Route, Routes } from "@solidjs/router";
-import { createHashHistory } from "history";
+import { HashRouter, Route } from "@solidjs/router";
 
 import "./scss/index.scss";
 import App from "./App";
@@ -10,19 +9,16 @@ import AddStory from "./components/addStoryForm";
 import OneStory from "./components/oneStory";
 
 const root = document.getElementById("root");
-const history = createHashHistory();
 
 render(
   () => (
-    <Router source={history}>
-      <Routes>
-        <Route path="/" component={App} />
-        <Route path="/create" component={AddStory} />
-        <Route path="/all" component={AllStories} />
-        <Route path="/one" component={OneStory} />
-        <Route path="/random" component={OneStory} />
-      </Routes>
-    </Router>
+    <HashRouter>
+      <Route path="/" component={App} />
+      <Route path="/create" component={AddStory} />
+      <Route path="/all" component={AllStories} />
+      <Route path="/one" component={OneStory} />
+      <Route path="/random" component={OneStory} />
+    </HashRouter>
   ),
   root
 );
